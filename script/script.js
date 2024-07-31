898ad9ae84c988455d43d811840cd4802b6bcb5e
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-07-31 14:57:46 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,31 @@
+/*
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+
+*Nota
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+Consigli:
+Fate la scaletta degli step e commentate man mano che scrivete il codice
+Usate i console.log per verificare che tutto sia a posto
+Disattivate live server: può darvi problemi coi prompt.
+
+Bonus
+-Stampare il prezzo finale sulla pagina invece che in console
+-Aggiungere, sempre in pagina, altre informazioni, come il prezzo prima dello sconto e l'età e il chilometraggio indicati dall'utente
+-Abbellire col CSS 
+*/
+
+/*
+
+Preparo i dati che mi servono 
+
+
+
+*/
+
