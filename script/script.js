bd26643a7f1bb5776785b74c34aa98f73f29a012
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-07-31 17:08:04 +0200

    Add Validation on User input. Removed unnecessary comments, add needed. Add the macrosteps at the start of js file.

--- script/script.js
+++ script/script.js
@@ -1,70 +1,72 @@
-/*
-Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-
-il prezzo del biglietto è definito in base ai km (0.21 € al km)
-
-va applicato uno sconto del 20% per i minorenni
-va applicato uno sconto del 40% per gli over 65.
-
-*Nota
-L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
-
-Consigli:
-Fate la scaletta degli step e commentate man mano che scrivete il codice
-Usate i console.log per verificare che tutto sia a posto
-Disattivate live server: può darvi problemi coi prompt.
-
-Bonus
--Stampare il prezzo finale sulla pagina invece che in console
--Aggiungere, sempre in pagina, altre informazioni, come il prezzo prima dello sconto e l'età e il chilometraggio indicati dall'utente
--Abbellire col CSS 
+/* 
+-Preparo gli elementi che voglio usare del DOM
+-setto variabili già note
+-Chiedo all'utente i 2 input richiesti
+-Controllo input inseriti dall'utente
+-elaboro i dati
+-mostro in pagina il risutato
 */
 
-/*
-
-Preparo i dati che mi servono 
-
-
-
-*/
 //Preparazione
-const kmPrice = 0.21 
- 
-//recupero gli elementi che voglio usare dalla pagina e la assegno a una variabile
+const kmPrice = 0.21;
+
+//recupero gli elementi che voglio usare dalla pagina e li assegno a una variabile
 const resultElement = document.getElementById('result');
 
 
-// Recupero i dati (userAge, userkm), dall'utente
+// Recupero i dati (userAge, userKm), dall'utente
 
-const userAge = parseInt(prompt('inserisci la tua età', '27'));
+const userAge = parseInt(prompt('Inserisci la tua età', '27'));
 console.log('userAge:', userAge);
 
-const userKm = parseInt(prompt('Per quanri km hai intenzione di viaggiare?', '100'));
+const userKm = parseInt(prompt('Per quanti km hai intenzione di viaggiare?', '100'));
 console.log('userKm:', userKm)
+
+// Fase validazione dati, prima di elaborarli
+
+const isInvalidAge = isNaN(userAge) || userAge < 1;
+console.log(isInvalidAge, 'invalidAge?');
+const isInvalidKm = isNaN(userKm) || userKm < 1;
+console.log(isInvalidKm, 'invalidKm?');
+
+if (isInvalidAge || isInvalidKm) {   // non è molto carino, se sbagli entrambi ti dirà solo che avri sbagliato il primo, in questo caso l'età
+    const errorMessage = isInvalidAge ? "Hai inserito un'età sbagliata!" : "Hai inserito un kilometraggio sbagliato!";
+    alert(errorMessage);
+    location.reload();   // se trovo errore, ricarico la pagina
+}
 
 // Fase Elaborazione dati 
 
+//metodo 1. tengo separate le condizioni dato che sono solo 2 e semplici, ma costringo ogni volta a farle entrambe
+/*
+ prezzo senza contare età. let così da modificarlo in seguito
 
-
-//metodo 1. tengo separate le condizioni dato che sono solo 2 e semplici
-// prezzo senza contare etò. let così da modificarlo in seguito
 let tripPrice = kmPrice * userKm;
 console.log('tripPrice:', tripPrice);
 
 if (userAge > 64) { // over 65
-    tripPrice = tripPrice -((tripPrice / 100) * 40);
+    tripPrice = tripPrice - ((tripPrice / 100) * 40);
     console.log('40-discount:', tripPrice);
 }
 if (userAge < 18) {  // minorenni
-    tripPrice = tripPrice -((tripPrice / 100) * 20);
+    tripPrice = tripPrice - ((tripPrice / 100) * 20);
+    console.log('20-discount', tripPrice);
+}
+*/
+
+// Metodo 2 Unisco le condizioni per usare una const
+
+const startPrice =  kmPrice * userKm;
+if (userAge > 17 && userAge < 65 ) {
+    tripPrice = startPrice
+} else if (userAge > 64) {
+    tripPrice = startPrice - ((startPrice / 100) * 40);
+    console.log('40-discount:', tripPrice);
+} else {
+    tripPrice = startPrice - ((startPrice / 100) * 20);
     console.log('20-discount', tripPrice);
 }
 
-// Metodo 2 Unisco le condizioni per usare una const
-/*
-num.toFixed([digits]) 
-*/
-
-
+// Fase di produzione output
 resultElement.innerText = `ecco il prezzo del tuo biglietto: ${tripPrice.toFixed(2)} euro.`;
+console.log(tripPrice, typeof(tripPrice));
